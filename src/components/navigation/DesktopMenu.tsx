1ebc29cdedf8f94b2ea7d3e3a5592d5cb1e78b5f
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-05-04 21:22:53 +0930

    added contact page

--- src/components/navigation/DesktopMenu.tsx
+++ src/components/navigation/DesktopMenu.tsx
@@ -1,10 +1,19 @@
 import { Group, MediaQuery } from '@mantine/core';
-import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const DesktopMenu: React.FC = () => {
+function DesktopMenu() {
   return (
     <MediaQuery smallerThan='sm' styles={{display: 'none'}}>
       <Group direction='row'>
+        <NavLink style={({ isActive }) => {
+          return {
+            color: isActive ? "blue" : ""
+          }
+        }}
+        to='/'
+        >
+          Home
+        </NavLink>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
         <a href="#Resume">Resume</a>
