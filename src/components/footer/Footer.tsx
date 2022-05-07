14c3e904f0557e3b2a8dd44d97a94a30b11d620f
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-05-07 23:16:50 +0930

    finished footer styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,16 +5,16 @@ import { BiMailSend } from "react-icons/bi";
 function Footer() {
 
   return(
-    <Foot height={'3rem'} mt='2rem'>
+    <Foot height={'3rem'} mt='2rem' style={{borderTop: 'none'}}>
       <Group align='center' position='center'>
-        <ActionIcon component="a" href="https://github.com/mattyd96" target='_blank'>
-          <FaGithub />
+        <ActionIcon component="a" href="https://github.com/mattyd96" target='_blank' size='xl' title="Gthub Link">
+          <FaGithub size='2rem' />
         </ActionIcon>
-        <ActionIcon component="a" href="https://www.linkedin.com/in/matthew-durflinger" target='_blank'>
-          <FaLinkedin />
+        <ActionIcon component="a" href="https://www.linkedin.com/in/matthew-durflinger" target='_blank' size='xl' title="LinkedIn Link">
+          <FaLinkedin size='2rem' />
         </ActionIcon>
-        <ActionIcon component="a" href="mailto:matthewdcodes@gmail.com" target='_blank'>
-          <BiMailSend />
+        <ActionIcon component="a" href="mailto:matthewdcodes@gmail.com" target='_blank' size='xl' title="Email Link">
+          <BiMailSend size='2rem' />
         </ActionIcon>
       </Group>
     </Foot>
