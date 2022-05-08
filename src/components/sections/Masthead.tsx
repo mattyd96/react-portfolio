8af58df113ed0200da46f1518171ef6418df48d6
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-05-08 16:36:32 +0930

    fix attempt for images not loading on gh pages

--- src/components/sections/Masthead.tsx
+++ src/components/sections/Masthead.tsx
@@ -8,7 +8,7 @@ function Masthead() {
     <section style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
       <Navbar />
       <section style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flexGrow: '1'}}>
-          <img src="/assets/heroLogo.svg" alt="MD logo" style={{flexGrow: 1, maxWidth: 'min(100%, 25rem)'}}/>
+          <img src="./assets/heroLogo.svg" alt="MD logo" style={{flexGrow: 1, maxWidth: 'min(100%, 25rem)'}}/>
           <div>
             <h2>Matthew Durflinger</h2>
             <p>Fullstack Development Portfolio</p>
